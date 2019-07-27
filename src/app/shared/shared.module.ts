ca21689fc05f2bbebd9563c1c62071b1a00472f2
nzbin <nzb329@163.com>
2019-07-27 09:38:54 +0800

    refactor: update files

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,9 +8,14 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgProgressModule } from '@ngx-progressbar/core';
 import { NgProgressRouterModule } from '@ngx-progressbar/router';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
+import { NgxPopperModule } from 'ngx-popper';
 
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { MiniProgressComponent } from './components/mini-progress/mini-progress.component';
+import { Text3dComponent } from './components/text3d/text3d.component';
 
 const THIRD_MODULES = [
   MaterialModule,
@@ -18,8 +23,16 @@ const THIRD_MODULES = [
   NgProgressModule,
   NgProgressRouterModule,
   NgSelectModule,
+  FormlyModule,
+  FormlyMaterialModule,
+  NgxPopperModule,
 ];
-const COMPONENTS = [BreadcrumbComponent, PageHeaderComponent];
+const COMPONENTS = [
+  BreadcrumbComponent,
+  PageHeaderComponent,
+  MiniProgressComponent,
+  Text3dComponent,
+];
 const DIRECTIVES = [];
 const PIPES = [];
 
