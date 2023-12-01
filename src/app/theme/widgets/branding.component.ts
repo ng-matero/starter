8cc57b19ce66f05de9c299754322707c8c4b3e80
nzbin <nzb329@163.com>
2023-12-01 09:35:08 +0800

    refactor: update files to 16.2.0

--- src/app/theme/widgets/branding.component.ts
+++ src/app/theme/widgets/branding.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <a class="d-inline-block text-nowrap r-full text-reset" href="/">
       <img src="./assets/images/matero.png" class="brand-logo align-middle m-2 r-full" alt="logo" />
-      <span class="align-middle f-s-16 f-w-500 m-x-8">MATERO</span>
+      <span class="align-middle f-s-16 f-w-500 m-x-8 hide-small">MATERO</span>
     </a>
   `,
   styles: [
@@ -13,6 +13,10 @@ import { Component } from '@angular/core';
       .brand-logo {
         width: 30px;
         height: 30px;
+      }
+
+      a {
+        text-decoration: none;
       }
     `,
   ],
